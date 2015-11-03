ae6b7f18436634717ccd0bd7b15a2ec0e7037bfa
jiteshk50 <jiteshk50@gmail.com>
2015-11-04 00:21:51 +0530

    removing external users intrusion

--- app/controllers/admin_user_controller.rb
+++ app/controllers/admin_user_controller.rb
@@ -1,6 +1,6 @@
 class AdminUserController < ApplicationController
 
- before_action :confirm_logged_in, :except => [:create] 
+ before_action :confirm_logged_in, :except => [:new,:create] 
   def index
   	@admin_users = AdminUser.sorted
     
